a35b13401631637d9cf7ee973405a1f57bc3a38c
Charan <charanbhc@Charans-MacBook-Air.local>
2025-07-25 20:25:59 +0530

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -6,7 +6,7 @@ from sentences import sentences
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+access_token_secret = os.getenv("ACCESS_SECRET")
 
 # Authenticate with Twitter
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
